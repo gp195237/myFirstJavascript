03772f441440f999feb6c6536c269fd3264c03c4
gp195237 <45366752+gp195237@users.noreply.github.com>
2018-12-18 08:48:20 -0700

    Added java script you can click on the buttons and get the text

---
+++ myScript.js
@@ -0,0 +1,20 @@
+function myFunction () {
+    document.getElementById ('demo').innerHTML = "Paragraph Changed";
+    
+}
+
+//Defining variables
+
+var x, y, z;
+x= 5;
+y= 6;
+z = x + y;
+
+//this function changes the text in the match paragraph
+
+function mathFunction () {
+    document.getElementById('math').innerHTML = z;
+    
+    
+}
+
